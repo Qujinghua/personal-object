3109a50fe0b78c4246606982e8f899573b71af2b
unknown <qujinghua@ESG.360ES.CN>
2018-08-24 19:51:08 +0800

    first commit

---
+++ src/views/videoexample/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="product in products">{{product}}</li>
+      </ul>
+    <!-- <h2>{{ product }} are in stock.</h2> -->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        'Boots',
+        'Jacket',
+        'Hiking Socks'
+      ]
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
