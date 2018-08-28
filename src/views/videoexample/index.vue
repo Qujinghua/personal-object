8f87a8331ca8f0e9473a2f0b20da572aadb903c3
渠景华 <893510847@qq.com>
2018-08-28 17:30:18 +0800

    引入element-ui

--- src/views/videoexample/index.vue
+++ src/views/videoexample/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="product in products">
-        <input type="number" v-model.number="product.quantity">
+  <div class="example">
+    <div class="example-content" v-for="product in products">
+      <el-input v-model.number="product.quantity" style="width:150px"></el-input>
+      <span>
         {{product.quantity}} {{ product.name }}
         <span v-if="product.quantity === 0">
-          - OUT OF STOCK
+          已售罄
         </span>
-        <button @click="product.quantity += 1">
-          Add
-        </button>
-      </li>
-    </ul>
+      </span>
+      
+      <el-button @click="product.quantity += 1">
+        Add
+      </el-button>
+      <el-button @click="product.quantity -= 1">
+        Reduce
+      </el-button>
+    </div>
     <h2>Total Inventory: {{ totalProducts }}</h2>
   </div>
 </template>
@@ -29,7 +33,7 @@ export default {
     }
   },
   components: {
-
+    
   },
   computed: {
     totalProducts () {
@@ -58,4 +62,18 @@ export default {
   }
 }
 </script>
+<style lang="less" scoped>
+.example {
+  &-content {
+    display: flex;
+    justify-content: center;
+    span:nth-child(2) {
+      display: block;
+      line-height: 40px;
+      width: 200px;
+    }
+  }
+}
+</style>
+
 
