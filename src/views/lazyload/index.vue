6a18e244221aebee2fed77e3b336ee5770c506ab
渠景华 <893510847@qq.com>
2018-08-30 20:21:03 +0800

    jquery懒加载

---
+++ src/views/lazyload/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lazyload">
+    <jquery-load></jquery-load>
+  </div>
+</template>
+<script>
+import jqueryLoad from './jquery.vue'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    jqueryLoad
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
