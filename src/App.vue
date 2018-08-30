9d4ee31d315ca3e2f13ba1e596bfc7f8494dd1b7
渠景华 <893510847@qq.com>
2018-08-30 20:20:10 +0800

    app.js

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/videoexample">videoexample</router-link>
+      <router-link to="/videoexample">videoexample</router-link> | 
+      <router-link to="/lazyload">lazyload</router-link>
     </div>
     <router-view/>
   </div>
