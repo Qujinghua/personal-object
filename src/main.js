09972823151c5cc95c49e993790aeda4920689c7
渠景华 <893510847@qq.com>
2018-08-31 19:20:13 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, VueAxios, axios)
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  // error: 'dist/error.png',
+  loading: './assets/loading.gif',
+  attempt: 1
+  // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+  // listenEvents: [ 'scroll' ]
+})
 // Vue.use
 require('./mock/mock.js')
 
