81804093a30852ea7592a48b187bbc1b8e390e30
渠景华 <893510847@qq.com>
2018-09-14 16:32:32 +0800

    父子组件通信

---
+++ src/views/PChild/child.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>这是子组件，接收的值为{{getTransmit}}</h2>
+    <el-button @click="change">子组件改变值为2</el-button>
+  </div>
+</template>
+<script>
+export default {
+  props: ['transmit'],
+  data () {
+    return {
+      getTransmit: this.transmit
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    change () {
+      this.getTransmit = 2
+      this.$emit('childChange', this.getTransmit)
+    }
+  }
+}
+</script>
---
+++ src/views/PChild/parent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>这是父组件<span v-if="childData">{{childData}}</span></h1>
+    <child :transmit="pData" v-on:childChange="childChangeValue"></child>
+  </div>
+</template>
+<script>
+import child from './child.vue'
+export default {
+  data () {
+    return {
+      pData: 1,
+      childData: null
+    }
+  },
+  components: {
+    child
+  },
+  methods: {
+    childChangeValue (data) {
+      this.childData = data
+    }
+  }
+}
+</script>
+
+
