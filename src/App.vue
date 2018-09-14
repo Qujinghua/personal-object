61985e9f3caae02dc0be724839bf7ff14a0e08eb
渠景华 <893510847@qq.com>
2018-09-14 16:32:12 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/videoexample">videoexample</router-link> | 
-      <router-link to="/lazyload">lazyload</router-link>
+      <router-link to="/lazyload">lazyload</router-link> |
+      <router-link to="/PChild">PChild</router-link>
     </div>
     <router-view/>
   </div>
